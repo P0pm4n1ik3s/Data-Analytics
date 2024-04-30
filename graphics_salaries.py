4e62e9c9cf6541f86e94c26b8f7a0f1545410507
P0pm4n1ik3s <155198076+P0pm4n1ik3s@users.noreply.github.com>
2024-04-30 19:51:29 +0200

    Update graphics_salaries.py

--- graphics_salaries.py
+++ graphics_salaries.py
@@ -3,6 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+#THE CODE BELOW ALLOW YOU TO READ THE 'SALARIES.CSV' FILE, FILTER DATA PER 'YEAR', 'EXPERIENCE LEVEL', 'EMPLOYMENT TYPE' AND 'COMPANY SIZE'.
+#IT GROUPS RESULTS BY 'JOB TITLE' AND 'WORK YEAR' AND PROVIDE THE AVERAGE 'SALARY IN USD'.
+#EVENTUALLY A BAR GRAPHIC GET CREATED SHOWING THE FINAL RESULT
+
 file = pd.read_csv(r"C:\Users\yuz3741\Desktop\Webucator\Python\Data_Analysis\salaries.csv")
 file_columns = file[['work_year', 'experience_level', 'employment_type', 'job_title', 'salary_in_usd', 'company_size']]
 file_filter = file_columns[(file_columns['work_year'] == 2023) & (file_columns['experience_level'] == 'EN') & (file_columns['employment_type'] == 'FT') & (file_columns['company_size'] == 'M')]
